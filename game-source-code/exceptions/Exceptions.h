0bb140d084b89e61af51094e54b2b9a707550957
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-01-24 00:37:07 +0200

    Added a file not found exception class
    Former-commit-id: d42860d46f78b272e906a2eb897869c6e0966fca

---
+++ game-source-code/exceptions/Exceptions.h
@@ -0,0 +1,19 @@
+/**
+ * @brief Defines possible exceptions that may be thrown in the game
+ */
+
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+#include <exception>
+#include <string>
+
+/**
+ * @brief Exception class for when a disk file cannot be found (e.g disk file containing high scores)
+ */
+class FileNotFound : public std::runtime_error{
+public:
+    explicit FileNotFound(const std::string& message) : std::runtime_error(message){}
+};
+
+#endif
