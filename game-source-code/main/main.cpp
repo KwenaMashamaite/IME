4e29b79dd27027073b86581d66b02f542bd57a54
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-01-24 01:03:43 +0200

    Added a simple resource manager class usage example
    Former-commit-id: dce3989d2ae6891305795bad9c07b6f53cc1f826

--- game-source-code/main/main.cpp
+++ game-source-code/main/main.cpp
@@ -1,15 +1,26 @@
 #include "gui/window/Window.h"
+#include "resources/ResourceManager.h"
 
 int main(){
     auto window = Gui::Window();
     window.create("duel invaders", 600u, 600u);
-    while(window.isOpen()){
+    auto resourceManager = ResourceManager();
+    auto background = sf::Sprite(resourceManager.getTexture("backgroundTwo.png"));
+    auto title = sf::Text("DUEL INVADERS", resourceManager.getFont("basson.ttf"), 50u);
+    title.setPosition(window.getDimensions().width / 2.0f - title.getGlobalBounds().width / 2.0f, 0.0f);
+    auto playerOne = sf::Sprite(resourceManager.getTexture("playerOne.png"));
+    playerOne.setPosition(window.getDimensions().width / 2.0f, window.getDimensions().height / 2.0f);
+
+    while (window.isOpen()){
         sf::Event event;
         while (window.pollEvent(event)){
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        window.clear(sf::Color::Magenta);
+        window.clear();
+        window.draw(background);
+        window.draw(title);
+        window.draw(playerOne);
         window.display();
     }
 
