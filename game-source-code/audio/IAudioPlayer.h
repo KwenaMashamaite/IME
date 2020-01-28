1323b1baed79c9031de65b2f646e56ad831003b9
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-01-28 13:25:48 +0200

    Added audio player interface
    Former-commit-id: d68e99562225a7cda2caa275834f1d01b666c6ec

---
+++ game-source-code/audio/IAudioPlayer.h
@@ -0,0 +1,36 @@
+/**
+ * @brief Interface for audio players
+ */
+
+#ifndef AUDIOPLAYER_H
+#define AUDIOPLAYER_H
+
+#include <string>
+
+namespace Audio {
+    class IAudioPlayer {
+    public:
+        /**
+         * @brief Play audio
+         * @param filename Filename of the audio to play
+         */
+        virtual void play(const std::string &filename) = 0;
+
+        /**
+         * @brief Stop the currently playing audio
+         */
+        virtual void stop() = 0;
+
+        /**
+         * @brief Pause the currently playing audio
+         */
+        virtual void pause() = 0;
+
+        /**
+         * @brief Resume paused audio
+         */
+        virtual void resume() = 0;
+    };
+}
+
+#endif
