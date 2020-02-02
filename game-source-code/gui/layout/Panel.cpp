2aa2c3aab3d5dd84c676fabaa2a5bef3199db01f
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-02-02 18:18:01 +0200

    Add layout manager positioning system
    Former-commit-id: e81e52798bc11122de2a11fbf13e628cb69b067a

--- game-source-code/gui/layout/Panel.cpp
+++ game-source-code/gui/layout/Panel.cpp
@@ -5,3 +5,20 @@ Gui::Panel::Panel(float x, float y){
     panel_.setSize(sf::Vector2f(0, 0));
     panel_.setPosition(x, y);
 }
+
+Dimensions Gui::Panel::getDimensions() const {
+    return {static_cast<unsigned int>(panel_.getGlobalBounds().width),
+            static_cast<unsigned int>(panel_.getGlobalBounds().height)};
+}
+
+void Gui::Panel::setDimensions(const Dimensions &dimensions) {
+    panel_.setSize(sf::Vector2f(dimensions.width, dimensions.height));
+}
+
+Position Gui::Panel::getPosition() const {
+    return {panel_.getPosition().x, panel_.getPosition().y};
+}
+
+void Gui::Panel::setPosition(const Position &position) {
+    panel_.setPosition(position.x, position.y);
+}
--- game-source-code/gui/layout/Panel.h
+++ game-source-code/gui/layout/Panel.h
@@ -20,6 +20,30 @@ namespace Gui {
          */
         Panel(float x, float y);
 
+        /**
+         * @brief Get the position of the panel
+         * @return Position of the panel
+         */
+        Position getPosition() const;
+
+        /**
+         * @brief Set the position of the panel
+         * @param position New position of the panel
+         */
+        void setPosition(const Position &position);
+
+        /**
+         * @brief Get the dimensions of the panel
+         * @return Dimensions of the panel
+         */
+        Dimensions getDimensions() const;
+
+        /**
+         * @brief Set the dimensions of the panel
+         * @param dimensions New dimensions of the panel
+         */
+        void setDimensions(const Dimensions &dimensions);
+
     private:
         //Representation
         sf::RectangleShape panel_;
