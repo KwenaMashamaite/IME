b9ca52801ead301691cd2ae11d53e1822926dc1c
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-02-02 16:53:41 +0200

    Add control element for displaying text
    Former-commit-id: e63af0ef880dae9f5fab2cc0c09be92e1ed7dbf9

---
+++ game-source-code/gui/control/TextBlock.h
@@ -0,0 +1,30 @@
+/**
+ * @brief Static UI element that can display multiple lines of text
+ */
+
+#ifndef TEXTBLOCK_H
+#define TEXTBLOCK_H
+
+#include "gui/control/UIElement.h"
+#include <string>
+
+namespace Gui {
+    class TextBlock : public UIElement {
+    public:
+        /**
+         * @brief Construct a new Text Block object
+         * @param labelText - text to be displayed
+         */
+        explicit TextBlock(const std::string &labelText);
+
+        /**
+         * @brief Construct a new TextBlock object
+         * @param content Text to be displayed
+         * @param font Font of the text
+         * @param textCharSize Character size of the text
+         */
+        TextBlock(const std::string &content, const std::string &font, unsigned int textCharSize);
+    };
+}
+
+#endif
--- game-source-code/main/main.cpp
+++ game-source-code/main/main.cpp
@@ -3,48 +3,37 @@
 #include "input/InputManager.h"
 #include "event/EventPublisher.h"
 #include "globals/Globals.h"
-#include "audio/SoundEffectPlayer.h"
-#include "audio/MusicPlayer.h"
+#include "gui/control/TextBlock.h"
+#include <iostream>
 
 int main(){
     auto window = Gui::Window();
     window.create("duel invaders", 600u, 600u);
     auto resourceManager = ResourceManager();
     auto background = sf::Sprite(resourceManager.getTexture("backgroundTwo.png"));
-    auto title = sf::Text("DUEL INVADERS", resourceManager.getFont("basson.ttf"), 50u);
-    title.setPosition(window.getDimensions().width / 2.0f - title.getGlobalBounds().width / 2.0f, 0.0f);
-    auto playerOne = sf::Sprite(resourceManager.getTexture("playerOne.png"));
-    playerOne.setPosition(window.getDimensions().width / 2.0f, window.getDimensions().height / 2.0f);
+    auto instructions = Gui::TextBlock("Destroy enemies before they invade planet earth\nand rule the world");
+    instructions.setFillColour(Gui::Colour{38, 130, 238, 0});
+    instructions.setTextCharSize(20);
+    instructions.setPosition(window.getDimensions().width / 2.0f - instructions.getDimensions().width / 2.0f, window.getDimensions().height / 2.0f);
     auto inputManager = InputManager();
 
-    ResourceManager::load(ResourceId::SOUND_BUFFER, {"invaderExplosion.wav", "playerExplosion.wav", "laserExplosion.wav"});
-    ResourceManager::load(ResourceId::MUSIC, "originalSpaceInvadersTrack.ogg");
+    Globals::Events::windowClose.addListener([&window](){
+        window.close();
+    });
 
-    auto soundEffectPlayer = Audio::SoundEffectPlayer();
-    auto musicPlayer = Audio::MusicPlayer();
-    musicPlayer.play("originalSpaceInvadersTrack.ogg");
-    musicPlayer.setLoop(true);
     while (window.isOpen()){
         EventPublisher::update(window);
 
         if(inputManager.isKeyPressed(InputManager::Key::A)){
-            soundEffectPlayer.play("invaderExplosion.wav");
+            std::cout << "you pressed A" << std::endl;
         }
         if(inputManager.isMouseButtonPressed(InputManager::MouseButton::RMouseButton)){
-            soundEffectPlayer.play("laserExplosion.wav");
+            std::cout << "You right clicked mouse" << std::endl;
         }
-        if(inputManager.isKeyPressed(InputManager::Key::P)){
-            musicPlayer.pause();
-        }
-        if(inputManager.isKeyPressed(InputManager::Key::R)){
-            musicPlayer.resume();
-        }
-
         inputManager.update();
         window.clear();
         window.draw(background);
-        window.draw(title);
-        window.draw(playerOne);
+        instructions.draw(window);
         window.display();
     }
 
