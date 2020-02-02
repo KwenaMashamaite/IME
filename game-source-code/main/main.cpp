a4207b5b675a4813abef74ae14c8759de1cf0f39
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-02-02 21:02:22 +0200

    Add a button
    Former-commit-id: e6408498ff1bafdac8b2dafd36f1b475e06f5191

--- game-source-code/main/main.cpp
+++ game-source-code/main/main.cpp
@@ -1,41 +1,60 @@
-#include "gui/window/Window.h"
-#include "resources/ResourceManager.h"
-#include "input/InputManager.h"
-#include "event/EventPublisher.h"
 #include "globals/Globals.h"
-#include "gui/control/TextBlock.h"
+#include "gui/window/Window.h"
 #include "gui/layout/StackPanel.h"
+#include "gui/control/Button.h"
+#include "event/EventPublisher.h"
+#include <ctime>
 #include <iostream>
 
 int main(){
+    srand(time(nullptr));
     auto window = Gui::Window();
-    window.create("duel invaders", 600u, 600u);
-    auto resourceManager = ResourceManager();
-    auto background = sf::Sprite(resourceManager.getTexture("backgroundTwo.png"));
-    auto play = std::make_shared<Gui::TextBlock>("PLAY");
-    auto exit = std::make_shared<Gui::TextBlock>("EXIT");
-    auto buttonPanel = Gui::StackPanel(window.getDimensions().width / 2.0f, window.getDimensions().height /2.0f, Gui::Orientation::Vertical);
-    buttonPanel.addElement(play);
-    buttonPanel.addElement(exit);
-    auto inputManager = InputManager();
+    window.create("test", 600, 600);
+
+    auto greetings = std::vector{"Sanibonani\n (isiZulu)", "Molo\n (isiXhosa)", "Hallo\n (Afrikaans)",
+                                 "Thobela\n (Sepedi)", "Dumela\n (Setswana)", "Lumela\n (Sesotho)",
+                                 "Abusheni\n (Xitsonga)", "Sanibona\n (SiSwati)", "Avuwani\n (Tshivenda)",
+                                 "Salibonani\n (isiNdebele)", "Hello\n (English)"};
+
+    auto fonts = std::vector{"sansation.ttf", "hangedLetters.ttf", "basson.ttf", "philosopher.ttf"};
+
+    auto charSize = 20;
+    auto languagesPanel = Gui::StackPanel(0.0f, window.getDimensions().height / 2.0f, Gui::Orientation::Vertical);
+    for (const auto& greeting : greetings) {
+        languagesPanel.addElement([&]() {
+            auto button = std::make_shared<Gui::Button>(greeting);
+            button->setTextCharSize(charSize);
+            button->setPadding(20.0f);
+            button->setMargin(1.0f);
+
+            button->setPosition(window.getDimensions().width / 2.0f - button->getDimensions().width / 2.0f,
+                                window.getDimensions().height / 2.0f);
+
+            button->mouseEnterEvent.addListener([button]() {
+                button->setTextFillColour({34,56, 231});
+                button->setFillColour({10, 67, 90});
+            });
+            button->mouseLeaveEvent.addListener([button]() {
+                button->setTextFillColour({54, 78, 3});
+                button->setFillColour({98, 88, 143});
+            });
+            button->clickEvent.addListener([&, button]() {
+                auto randonIndex = rand() % (fonts.size() - 1);
+                button->setTextFont(fonts.at(randonIndex));
+            });
+            return button;
+        }());
+    }
 
     Globals::Events::windowClose.addListener([&window](){
         window.close();
     });
 
-    while (window.isOpen()){
-        EventPublisher::update(window);
-
-        if(inputManager.isKeyPressed(InputManager::Key::A)){
-            std::cout << "you pressed A" << std::endl;
-        }
-        if(inputManager.isMouseButtonPressed(InputManager::MouseButton::RMouseButton)){
-            std::cout << "You right clicked mouse" << std::endl;
-        }
-        inputManager.update();
+    auto emitter = EventPublisher();
+    while (window.isOpen()) {
+        emitter.update(window);
         window.clear();
-        window.draw(background);
-        buttonPanel.draw(window);
+        languagesPanel.draw(window);
         window.display();
     }
 
