89ca00a8fae66248c3167702630b94eba71ef0e5
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-02-04 18:52:57 +0200

    Fix class not deregistering from events on destruction
    This might cause the program to crash as a non existent handler will be
    invoked
    
    
    Former-commit-id: 9e1e7702d8df8aae5e5cb6de602cf37d37ea0e25

--- game-source-code/gui/control/ClickableUIElement.cpp
+++ game-source-code/gui/control/ClickableUIElement.cpp
@@ -1,6 +1,9 @@
 #include "ClickableUIElement.h"
 #include "globals/Globals.h"
 #include "input/InputManager.h"
+#include "event/IdHolder.h"
+
+using Utility::IdHolder;
 
 using Globals::Events;
 
@@ -29,17 +32,21 @@ void Gui::ClickableUIElement::onHoverLost() {
 }
 
 void Gui::ClickableUIElement::subscribeToEvents() {
-    Events::mouseMoved.addListener([this](int x, int y) {
+    IdHolder::add("mouseMoved", Events::mouseMoved.addListener([this](int x, int y) {
         if (contains(x, y) && !isSelected_)
             onHover();
         else if (!contains(x, y) && isSelected_)
             onHoverLost();
-    });
+    }));
 
-    Events::mouseButtonReleased.addListener([this](InputManager::MouseButton releasedMouseButton, int x, int y) {
-        if (isSelected_ && releasedMouseButton == InputManager::MouseButton::LMouseButton)
-            onClick();
-    });
+    IdHolder::add("mouseButtonPressed", Events::mouseButtonReleased.addListener(
+            [this](InputManager::MouseButton releasedMouseButton, int x, int y){
+                if (isSelected_ && releasedMouseButton == InputManager::MouseButton::LMouseButton)
+                    onClick();
+    }));
 }
 
-Gui::ClickableUIElement::~ClickableUIElement() = default;
+Gui::ClickableUIElement::~ClickableUIElement(){
+    Events::mouseMoved.removeListener(IdHolder::getIdFor("mouseMoved"));
+    Events::mouseButtonReleased.removeListener(IdHolder::getIdFor("mouseButtonPressed"));
+}
