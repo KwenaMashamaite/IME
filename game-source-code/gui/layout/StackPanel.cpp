3ab334ae9805892822e664af4f4a5b53edbc0ade
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-02-06 18:40:50 +0200

    Fix panel not accommodating element with largest dimensions
    The panel assumed the dimensions of the first element that was added,
    When a new element with dimensions bigger than those of the first
    element was added, it did not adjust its size to accommodate this new
    dimensions, as a result some elements extended beyond the perimeter of
    the panel
    
    
    Former-commit-id: a10388e6d9ee0408a94bbe3640ee92d5701c46e1

--- game-source-code/gui/layout/StackPanel.cpp
+++ game-source-code/gui/layout/StackPanel.cpp
@@ -11,16 +11,21 @@ void Gui::StackPanel::addElement(std::shared_ptr<UIElement> guiElement) {
             setDimensions({guiElement->getDimensions().width, guiElement->getDimensions().height});
             guiElement->setPosition(getPosition().x, getPosition().y);
         }else {
+            auto newWidth = 0u, newHeight = 0u;
             switch (orientation_){
                 case Orientation::Vertical:
                     guiElement->setPosition(lastInsertedElement_->getPosition().x, lastInsertedElement_->getPosition().y
                                             + lastInsertedElement_->getDimensions().height);
-                    setDimensions({getDimensions().width, getDimensions().height + guiElement->getDimensions().height});
+                    newWidth = guiElement->getDimensions().width > lastInsertedElement_->getDimensions().width
+                               ? guiElement->getDimensions().width : lastInsertedElement_->getDimensions().width;
+                    setDimensions({newWidth, getDimensions().height + guiElement->getDimensions().height});
                     break;
                 case Orientation::Horizontal:
                     guiElement->setPosition(lastInsertedElement_->getPosition().x + lastInsertedElement_->getDimensions().width,
                                             lastInsertedElement_->getPosition().y);
-                    setDimensions({getDimensions().width + guiElement->getDimensions().width, getDimensions().height});
+                    newHeight = guiElement->getDimensions().height > lastInsertedElement_->getDimensions().height
+                               ? guiElement->getDimensions().height : lastInsertedElement_->getDimensions().height;
+                    setDimensions({getDimensions().width + guiElement->getDimensions().width, newHeight});
                     break;
             }
         }
