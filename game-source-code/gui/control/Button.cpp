50d17f4d48f62aaea5c8afcfe30a00a837fd68f1
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-02-10 16:41:52 +0200

    Remove intermediate base class
    This class is unnecessary because ONLY buttons are clickable, so it
    doesn't make any sense to have a clickable UI element base class.
    
    
    Former-commit-id: a1708e57eaa20bb59208a92fbfb6137f0f0763b7

--- game-source-code/gui/control/Button.cpp
+++ game-source-code/gui/control/Button.cpp
@@ -1,8 +1,47 @@
 #include "Button.h"
+#include "event/IdHolder.h"
+#include "input/InputManager.h"
+#include "globals/Globals.h"
 
-Gui::Button::Button(const std::string &buttonText) : ClickableUIElement(buttonText)
-{}
+using Utility::IdHolder;
+using Globals::Events;
+
+Gui::Button::Button(const std::string &buttonText)
+    : UIElement(buttonText),
+      isSelected_(false)
+{
+    subscribeToEvents();
+}
 
 Gui::Button::Button(const std::string &content, const std::string &font, unsigned int textCharSize)
-        : ClickableUIElement(content, font, textCharSize)
-{}
+    : UIElement(content, font, textCharSize),
+      isSelected_(false)
+{
+    subscribeToEvents();
+}
+
+void Gui::Button::subscribeToEvents() {
+    IdHolder::add("mouseMoved", Events::mouseMoved.addListener(
+        [this](int x, int y) {
+            if (contains(x, y) && !isSelected_) {
+                isSelected_ = true;
+                mouseEnterEvent.notifyListeners();
+            }else if (!contains(x, y) && isSelected_) {
+                isSelected_ = false;
+                mouseLeaveEvent.notifyListeners();
+            }
+        }
+    ));
+
+    IdHolder::add("mouseButtonPressed", Events::mouseButtonReleased.addListener(
+        [this](InputManager::MouseButton releasedMouseButton, int x, int y){
+            if (isSelected_ && releasedMouseButton == InputManager::MouseButton::LMouseButton)
+                clickEvent.notifyListeners();
+        }
+    ));
+}
+
+Gui::Button::~Button() {
+    Events::mouseMoved.removeListener(IdHolder::getIdFor("mouseMoved"));
+    Events::mouseButtonReleased.removeListener(IdHolder::getIdFor("mouseButtonPressed"));
+}
--- game-source-code/gui/control/Button.h
+++ game-source-code/gui/control/Button.h
@@ -1,16 +1,16 @@
 /**
- * @brief UI element that can be interacted with (e.g clicked). A callback function can be registered
- *        in order to be notified when the button is hovered over, clicked or lost focus (mouse left button)
+ * @brief UI element that can be interacted with (e.g clicked)
  */
 
 #ifndef GUIBUTTON_H
 #define GUIBUTTON_H
 
-#include "ClickableUIElement.h"
+#include "gui/control/UIElement.h"
+#include "event/Event.h"
 #include <string>
 
 namespace Gui {
-    class Button : public ClickableUIElement {
+    class Button : public UIElement {
     public:
         /**
          * @brief Construct a new Button object
@@ -22,12 +22,46 @@ namespace Gui {
         explicit Button(const std::string &buttonText);
 
         /**
-         * @brief Creates a gui button with the dimensions of the button text
+         * @brief Create a button
          * @param content Text to be displayed on the button
          * @param font  Font used for the text
          * @param textCharSize Character size of the text
+         *
+         * The button has the dimensions of the button content and
+         * a position of (0, 0) by default
          */
         Button(const std::string &content, const std::string &font, unsigned int textCharSize);
+
+        /**
+         * @brief Destructor
+         */
+        ~Button();
+
+    private:
+        /**
+         * @brief Subscribe to mouse click and mouse move events
+         */
+        void subscribeToEvents();
+
+    public:
+        /**
+         * @brief Subscribe to get notified when the button is clicked
+         */
+        Event<> clickEvent;
+
+        /**
+         * @brief Subscribe to get notified when the mouse enters the button
+         */
+        Event<> mouseEnterEvent;
+
+        /**
+         * @brief Subscribe to get notified when the mouse leaves the button
+         */
+        Event<> mouseLeaveEvent;
+
+    private:
+        //Selection state
+        bool isSelected_;
     };
 }
 
