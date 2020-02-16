3b820ca02262517c224926a73bbf3b02947bd731
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-02-16 23:55:19 +0200

    Add the ability to read data from a file on the disk
    Former-commit-id: efb4291af0dab5dea5e285455cb94b80784fae1f

---
+++ game-source-code/utility/FileReader.cpp
@@ -0,0 +1,10 @@
+#include "utility/FileReader.h"
+#include "exceptions/Exceptions.h"
+
+void Utility::FileReader::readFileInto(std::stringstream& buffer, const std::string&& filename){
+    inFile_.open(filename);
+	if(!inFile_.good())
+		throw FileNotFound("Cannot find file, " + filename);
+	buffer << inFile_.rdbuf(); //Read file content
+	inFile_.close();
+}
