9f11f992bf0b60301cf326b02cf70be893f43104
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-03-19 16:58:42 +0200

    Fix compilation errors
    Former-commit-id: 2944251988c3feb3aeb55a5153242b2b5de1a872

--- game-source-code/gui/drawer/Drawer.cpp
+++ game-source-code/gui/drawer/Drawer.cpp
@@ -10,22 +10,35 @@ Gui::Drawer::Drawer(Gui::Window &renderTarget) : renderTarget_(renderTarget)
 void Gui::Drawer::drawEntity(const std::shared_ptr<Entity> &entity) {
     if(entity && entity->isAlive()) {
         auto entitySprite = sf::Sprite(ResourceManager::getTexture(entity->getTexture()));
-        if (entity->getAngleOfRotation() != 0.0f) {
-            entitySprite.setOrigin(
-                entitySprite.getGlobalBounds().width,
-				entitySprite.getGlobalBounds().height
-            );
-            entitySprite.rotate(entity->getAngleOfRotation());
-        } else {
-            entitySprite.setOrigin(0.0f, entitySprite.getGlobalBounds().height);
+        entitySprite.setOrigin(entitySprite.getGlobalBounds().width,
+            entitySprite.getGlobalBounds().height);
+
+        switch (entity->getDirection()) {
+            case IEntity::Direction::Left:
+                entitySprite.rotate(-90.0f);
+                break;
+            case IEntity::Direction::Right:
+                entitySprite.rotate(90.0f);
+                break;
+            case IEntity::Direction::Up:
+                entitySprite.setOrigin(0.0f, entitySprite.getGlobalBounds().height);
+                break;
+            case IEntity::Direction::Down:
+                entitySprite.rotate(180.0f);
+                break;
         }
+
         entitySprite.setPosition(entity->getPosition().x, entity->getPosition().y);
         renderTarget_.draw(entitySprite);
     }
 }
 
 void Gui::Drawer::drawEntities(const std::initializer_list<std::shared_ptr<Entity>> &entities) {
-    std::for_each(entities.begin(), entities.end(), &Drawer::drawEntity);
+    std::for_each(entities.begin(), entities.end(),
+        [this](const std::shared_ptr<Entity> &entity){
+            drawEntity(entity);
+        }
+    );
 }
 
 void Gui::Drawer::drawBackground(const std::string &backgroundTexture) {
