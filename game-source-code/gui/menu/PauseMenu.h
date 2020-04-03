b1fb2061ad21e31ce43f9167d4505315d527a539
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-04-03 22:34:56 +0200

    Add missing public interface documentation for PauseMenu
    Former-commit-id: 70146faecd79bd5537754d29c7e0348a9137bfeb

--- game-source-code/gui/menu/PauseMenu.h
+++ game-source-code/gui/menu/PauseMenu.h
@@ -12,17 +12,33 @@
 namespace Gui {
     class PauseMenu : public IMenu{
     public:
-        explicit PauseMenu(Window &renderTarget, SystemEventEmitter& systemEventEmitter);
+        /**
+         * @brief Create a new Pause Menu object
+         * @param renderTarget Target to draw menu on
+         * @param systemEventEmitter System events emitter
+         */
+        PauseMenu(Window &renderTarget, SystemEventEmitter& systemEventEmitter);
 
+        /**
+         * @brief Draw all GUI elements and layouts of the menu
+         *        on a render target
+         */
         void draw() override;
 
+        /**
+         * @brief Remove all Gui elements and layouts of the menu from a
+         *        render target
+         */
         void clear() override;
 
     private:
+        //Games render target
         Window& renderTarget_;
+        //Layout panel for the menus UI elements
         std::unique_ptr<StackPanel> pauseMenuButtonsPanel_;
+        //Menu clear state
         bool isCleared_;
     };
 }
 
-#endif //SFML_2_5_0_GAME_DEV_TEMPLATE_PAUSEMENU_H
+#endif
