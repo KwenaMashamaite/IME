e3feca8eb5a2f77d979762057a54ff247522c9c8
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-04-03 20:26:51 +0200

    Strengthen the condition for adding gui elements to Canvas panel
    Previously only the place of placement of the UI element was considered.
    This means that UI elements added to the Canvas were allowed to extend outside
    the borders of the panel. Now the dimensions of the UI element are considered such
    that a UI element added to a Canvas will lie entirely within the Canvas
    
    
    Former-commit-id: 8c3bd53df8e5e94fd569939834332ae042d03882

--- game-source-code/gui/layout/Canvas.cpp
+++ game-source-code/gui/layout/Canvas.cpp
@@ -3,13 +3,15 @@
 Gui::Canvas::Canvas(float x, float y) : Panel(x, y) {}
 
 void Gui::Canvas::addElement(const std::string &alias, std::unique_ptr<UIElement> guiElement) {
-    auto elementOverlapsWithPanel = [&](){
+    auto isElementInsidePanel = [&](){
         return guiElement->getPosition().x >= getPosition().x
                && guiElement->getPosition().x <= getPosition().x + getDimensions().width
                && guiElement->getPosition().y >= getPosition().y
-               && guiElement->getPosition().y <= getPosition().y + getDimensions().height;
-    };
+               && guiElement->getPosition().y <= getPosition().y + getDimensions().height
+               && guiElement->getDimensions().width <= getDimensions().width
+               && guiElement->getDimensions().height <= getDimensions().height;
+    }();
 
-    if (elementOverlapsWithPanel())
+    if (isElementInsidePanel)
         add(alias, std::move(guiElement));
 }
