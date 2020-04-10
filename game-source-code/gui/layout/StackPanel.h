1450f351151bdb17c008f8b5a1f689d67f489d82
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-04-11 01:02:43 +0200

    Format documentation for StackPanel class
    Former-commit-id: 2acf894b781c818aad449315dc27d69425b5c849

--- game-source-code/gui/layout/StackPanel.h
+++ game-source-code/gui/layout/StackPanel.h
@@ -19,7 +19,8 @@ namespace Gui {
          * @brief Create a new Stack Panel object
          * @param x X coordinate of the panel
          * @param y Y coordinate of the panel
-         * @param orientation Specify how the UI elements are stacked to each other
+         * @param orientation Specify how the UI elements
+         *        are stacked to each other
          */
         StackPanel(float x, float y, Orientation orientation);
 
@@ -28,32 +29,36 @@ namespace Gui {
          * @param alias Name that can be used to refer to element
          * @param guiElement Element to be added
          */
-        void addElement(const std::string &alias, std::unique_ptr<UIElement> guiElement) override;
+        void addElement(const std::string &alias,
+                std::unique_ptr<UIElement> guiElement) override;
 
     private:
         //Panel orientation state
         bool isOrientationVertical_;
 
         /**
-         * @brief Set the position of a UI element relative to another UI element
+         * @brief Set the position of a UI element relative to
+         *        another UI element
          * @param uiElem UI element to position relative to another
          * @param refUIElem Reference UI element
          *
-         * The element whose position is changed will have the same orientation
-         * (vertical or horizontal) as the element which is used as reference.
+         * The element whose position is changed will have the
+         * same orientation (vertical or horizontal) as the
+         * element which is used as reference.
          */
         void setElemPosRelativeTo(const std::unique_ptr<UIElement> &uiElem,
                                   const std::unique_ptr<UIElement> &refUIElem);
 
         /**
-         * @brief Adjust the dimensions of the panel based on the dimensions of
-         *        a UI element
-         * @param uiElement UI Element to accomodate
+         * @brief Adjust the dimensions of the panel based on
+         *        the dimensions of a UI element
+         * @param uiElement UI Element to accommodate
          *
-         * This function will adjust the width or height of the panel depending
-         * on the dimensions of the UI element. The dimesnions of the panel will
-         * be adjusted such that all the egdes of the UI element lie within the
-         * edges of the panel
+         * This function will adjust the width or height of the
+         * panel depending
+         * on the dimensions of the UI element. The dimensions of
+         * the panel will be adjusted such that all the edges of
+         * the UI element lie within the edges of the panel
          */
         void adjustPanelDimensions(const std::unique_ptr<UIElement> &uiElement);
     };
