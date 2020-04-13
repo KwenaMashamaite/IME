ddbd0a1f6e049735dce9793404116fcfd1fc94d1
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-04-13 14:05:33 +0200

    Remove comparison of pointer with nullptr
    Former-commit-id: d653306321000f82fb1114c4afbbc00bf3dce85f

--- game-source-code/audio/MusicPlayer.cpp
+++ game-source-code/audio/MusicPlayer.cpp
@@ -3,26 +3,26 @@
 
 void Audio::MusicPlayer::play(const std::string &song){
     song_ = ResourceManager::getMusic(song);
-    if (song_ != nullptr)
+    if (song_)
         song_->play();
 }
 
 void Audio::MusicPlayer::setLoop(bool isLoop){
-    if(song_ != nullptr)
+    if(song_)
         song_->setLoop(isLoop);
 }
 
 void Audio::MusicPlayer::pause(){
-    if(song_ != nullptr)
+    if(song_)
         song_->pause();
 }
 
 void Audio::MusicPlayer::resume(){
-    if(song_ != nullptr && song_->getStatus() == sf::Music::Status::Paused)
+    if(song_ && song_->getStatus() == sf::Music::Status::Paused)
         song_->play();
 }
 
 void Audio::MusicPlayer::stop() {
-    if (song_ != nullptr)
+    if (song_)
         song_->stop();
 }
