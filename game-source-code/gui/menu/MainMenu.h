a82b24ada8e798743529464440a44ac258939912
Kwena <Mashamaite>
2020-04-16 16:32:48 +0200

    Update doxygen documentation for Main Menu class
    Former-commit-id: 88d247ffbe13a6f50eb8c8905c12b723e9912e3e

--- game-source-code/gui/menu/MainMenu.h
+++ game-source-code/gui/menu/MainMenu.h
@@ -39,27 +39,46 @@ namespace Gui {
         void initOnClickInfo();
 
         /**
-         * @brief Create the main menu title panel
+         * @brief Create the main menu title
+         *
+         * The title is positioned at the top of the window
          */
         void createTitle();
 
         /**
          * @brief Create the main menu footer
+         *
+         * The footer is positioned at the bottom of the
+         * window
          */
         void createFooter();
 
         /**
          * @brief Create the main menu navigation panel
+         *
+         * This function creates buttons that are stacked vertically and
+         * positioned at the left edge of the window. This buttons may be
+         * used to navigate the main menu
          */
         void createNavigationButtons();
 
         /**
          * @brief Initialize the main menu buttons
+         *
+         * This function defines how the navigation buttons responds to
+         * the mouse cursor. That is, what the buttons do when they are
+         * clicked or when the mouse cursor enters/leaves them
          */
         void initNavigationButtonActions();
 
         /**
          * @brief Create a button that return back to the main menu
+         *
+         * This function creates a button whose sole purpose is to return
+         * to the main menu after it hs been exited. This button is only
+         * visible if the main menu is exited. For example, clicking on
+         * the "Controls" button to see how the game is played would usually
+         * exit the main menu
          */
         void createReturnButton();
 
@@ -78,6 +97,9 @@ namespace Gui {
         /**
         * @brief Set text to be displayed on the info panel
         * @param newInfo Text to be set
+         *
+         * This function will overwrite any information that was on the info
+         * panel, prior to calling the function
         */
         void updateInfoPanel(const std::string& newInfo);
 
