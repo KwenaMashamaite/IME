193db0e07ff860ffc7e59029d4c2e6d6b3d09630
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-04-27 13:48:18 +0200

    Add event emitter unit tests
    Former-commit-id: b54ba07f6d282bafbed12321539a4e977ac5349e

---
+++ test-source-code/main.cpp
@@ -0,0 +1,13 @@
+/**
+ * @brief Entry point for running unit tests
+ *
+ * This file does not define any tests, all it does is run them.
+ * Unit tests are defined in a seperate file for each unit under
+ * test and included here to be run
+ */
+
+//Initialize Doctest unit testing framework
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+//Include unit tests to be run
+#include "../test-source-code/EventEmitterTests.hpp"
