f9b420f484fdf9165a4298836efe233c85cd702e
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-04-28 01:05:46 +0200

    Update doxygen documentation for the constructor and destructor of the window class
    Former-commit-id: af1c08cf25f0613e9e2918eb31ba583b21ebb3af

--- game-source-code/gui/window/Window.h
+++ game-source-code/gui/window/Window.h
@@ -17,8 +17,11 @@ namespace Gui {
     class Window : sf::NonCopyable {
     public:
         /**
-         * @brief Default constructor. Ensures only a single
-         *         instance of the class can exist at a time
+         * @brief Constructor
+         *
+         * @warning Only a single instance of the class can exist
+         * at a time. Attempting to instantiate the class while
+         * there is an active instance will terminate the program
          */
         Window();
 
@@ -113,9 +116,10 @@ namespace Gui {
         }
 
         /**
-         * @brief Destructor. Ensures a new Window instance can be
-         *        created when an existing Window instance is
-         *        destroyed
+         * @brief Destructor.
+         *
+         * Ensures a new Window instance can be created when an existing
+         * Window instance is destroyed
          */
         ~Window();
 
