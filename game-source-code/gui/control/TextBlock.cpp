35cae5f6f06120ef41f11604c2101c142a2877bd
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-05-01 17:19:29 +0200

    Customize Text Block to have a white background and black foreground by default
    Former-commit-id: baa6364540c74e06c014d2f6f33a5cf61a465631

--- game-source-code/gui/control/TextBlock.cpp
+++ game-source-code/gui/control/TextBlock.cpp
@@ -2,4 +2,7 @@
 
 Gui::TextBlock::TextBlock(const std::string &textContent)
     : UIElement(textContent)
-{}
+{
+    setFillColour({255, 255, 255}); //White
+    setTextFillColour({0, 0, 0}); //Black
+}
