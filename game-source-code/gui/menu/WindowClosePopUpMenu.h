613065193b53cc6114bb4a2565268faf40ebcf04
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-06-30 03:18:35 +0200

    Add a confirmation menu when closing the application window
    Former-commit-id: 4cb26bfe53d39443821895ba9b248ffea62a389b

---
+++ game-source-code/gui/menu/WindowClosePopUpMenu.h
@@ -0,0 +1,64 @@
+/**
+ * @brief Confirmation menu when a request to close window is made
+ */
+
+#ifndef WINDOWCLOSEPOPUPMENU_H
+#define WINDOWCLOSEPOPUPMENU_H
+
+#include "IMenu.h"
+#include "../layout/StackPanel.h"
+#include <unordered_map>
+#include <memory>
+#include <string>
+
+namespace Gui {
+    class WindowClosePopUpMenu : public IMenu {
+    public:
+        /**
+         * @brief Create a new pop-up Menu object
+         * @param renderTarget Target to draw menu on
+         */
+        explicit WindowClosePopUpMenu(Window& renderTarget);
+
+        /**
+         * @brief Draw menu on render target
+         * @param renderTarget Target to draw menu on
+         */
+        void draw(Window &renderTarget) override;
+
+        /**
+         * @brief Hide menu from a render target
+         *
+         * A hidden menu will not be drawn on a render target when calling the
+         * function draw(Window). Also, A hidden menu cannot be interacted with
+         * graphically. That is, interaction with the mouse cursor is disabled
+         * while the menu is hidden. However, some states of the menu may be
+         * changed while it is hidden but they will not be reflected graphically
+         */
+        void hide() override;
+
+        /**
+         * @brief Reveal a hidden menu
+         *
+         * @note This function does not show the menu on a render target, it only
+         * allows the menu to be drawn when calling draw(Window). @see hide()
+         */
+        void show() override;
+
+        /**
+         * @brief Check if menu is hidden or not
+         * @return True if menu is hidden, otherwise false
+         */
+        bool isHidden() const override;
+
+    private:
+        //Reference to a render target
+        Window& renderTarget_;
+        //Menu panels
+        StackPanel panel_;
+        //Visibility state
+        bool isHidden_;
+    };
+}
+
+#endif
--- game-source-code/main/main.cpp
+++ game-source-code/main/main.cpp
@@ -1,25 +1,19 @@
 #include "gui/window/Window.h"
-#include "gui/menu/MainMenu.h"
-#include "gui/control/Button.h"
-#include "gui/menu/PauseMenu.h"
-
-using Gui::StackPanel;
-using Gui::Button;
+#include "gui/menu/WindowClosePopUpMenu.h"
 
 int main(){
     auto window = Gui::Window();
     window.create("test", 800, 600);
-    auto mainMenu = Gui::MainMenu(window);
 
     //Close the window when the windows close button is clicked
     window.addEventListener("Closed", Callback<>([&window] {
-        window.close();
+        static auto closeConfirmationMenu = Gui::WindowClosePopUpMenu(window);
+        window.draw(closeConfirmationMenu);
     }));
 
     while (window.isOpen()) {
         window.processEvents();
         window.clear();
-        window.draw(mainMenu);
         window.display();
     }
 
