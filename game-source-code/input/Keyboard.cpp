298c873dcdb14eab12fb05367e7bf4560787a649
Kwena Mashamaite <1125650@students.wits.ac.za>
2020-07-02 13:21:08 +0200

    Add function to perform an action when a specific key is held down or pressed
    Former-commit-id: 8521071922ec4c45aff127be4b15f591f00328be

--- game-source-code/input/Keyboard.cpp
+++ game-source-code/input/Keyboard.cpp
@@ -1,9 +1,29 @@
 #include "Keyboard.h"
+#include "gui/window/Window.h"
 #include <SFML/Graphics.hpp>
 
+Keyboard::Keyboard() {
+    initEventEmitter();
+}
+
 bool Keyboard::isKeyPressed(Key keyId){
-    auto isKeyCurrentlyPressed = sf::Keyboard::isKeyPressed(static_cast<sf::Keyboard::Key>(keyId));
-    auto wasKeyPressed = prevKeyStateMap_[keyId];
-    prevKeyStateMap_[keyId] = isKeyCurrentlyPressed;
-    return isKeyCurrentlyPressed && !wasKeyPressed;
+    return sf::Keyboard::isKeyPressed(static_cast<sf::Keyboard::Key>(keyId));
+}
+
+void Keyboard::initEventEmitter() {
+    //A key is considered down if it is pressed and remains pressed
+    Gui::Window::addEventListener("keyPressed", Callback<Key>([](Key key){
+        auto wasKeyPressed = prevKeyStateMap_[key];
+        if(wasKeyPressed)
+            eventEmitter_.emit(std::to_string(static_cast<int>(key)) + "Down");
+        prevKeyStateMap_[key] = true;
+    }));
+
+    //A key is considered pressed if it was down and released
+    Gui::Window::addEventListener("keyReleased", Callback<Key>([](Key key){
+        eventEmitter_.emit(std::to_string(static_cast<int>(key)) + "Pressed");
+        prevKeyStateMap_[key] = false;
+    }));
+
+    isEventEmitterInitialized_ = true;
 }
--- game-source-code/input/Keyboard.h
+++ game-source-code/input/Keyboard.h
@@ -3,6 +3,7 @@
 #define KEYBOARD_H
 
 #include <unordered_map>
+#include "event/EventEmitter.h"
 
 class Keyboard{
 public:
@@ -39,18 +40,80 @@ public:
     };
 
     /**
+     * @brief Constructor
+     */
+    Keyboard();
+
+    /**
      * @brief Check if key is pressed or not.
      * @param key Key to check.
      * @return true if key is pressed, false if it not pressed.
      *
-     * A key is considered pressed if it currently pressed but it was
-     * not pressed prior to function call
+     * This function checks the state of a key in real time
      */
     static bool isKeyPressed(Key key);
+
+    /**
+      * @brief Add an event listener to a key down event
+      * @tparam Args Template parameter pack name
+      * @param key Key to listen for
+      * @param callback Function to execute when the key is down
+      * @return Event listener's identification number
+      *
+      * A key down event will fire if a key is pressed and remains pressed.
+      * The event will not fire If the key is pressed and immediately released
+      * @see onKeyPressed(Key, Callback) function
+      */
+    template <typename...Args>
+    static int onKeyDown(Key key, Callback<Args...> callback) {
+        if (!isEventEmitterInitialized_) //Function accessed without instantiation
+            initEventEmitter();
+        eventEmitter_.addEventListener(std::to_string(static_cast<int>(key)) + "Down", callback);
+    }
+
+    /**
+      * @brief Add an event listener to a key press event
+      * @tparam Args Template parameter pack name
+      * @param key Key to listen for
+      * @param callback Function to execute when the key is pressed
+      * @return Event listener's identification number
+      *
+      * A key pressed event fires when a key is pressed and immediately released
+      * or after a "keyDown" event (held key is released)
+      */
+    template <typename...Args>
+    static int onKeyPressed(Key key, Callback<Args...> callback) {
+        if (!isEventEmitterInitialized_) //Function accessed without instantiation
+            initEventEmitter();
+        eventEmitter_.addEventListener(std::to_string(static_cast<int>(key)) + "Pressed", callback);
+    }
+
+    /**
+      * @brief  Remove a listener from a window event
+      * @param  event Event to remove listener from
+      * @param  listenerId Identification number of the listener
+      *         to be removed
+      * @return True if a listener was removed from an event,
+      *         false if the specified event does not have a
+      *         listener with the specified id
+       */
+    static bool removeEventListener(const std::string& event, int callbackId){
+        return eventEmitter_.removeEventListener(event, callbackId);
+    }
+
+private:
+    /**
+     * @brief Initialize the event emitter for event publishing
+     */
+    static void initEventEmitter();
 
 private:
     //Holds the state of a key when it was last checked
     inline static std::unordered_map<Key, bool> prevKeyStateMap_{};
+    //Event Emitter/publisher
+    inline static EventEmitter eventEmitter_{};
+    //State of the event emitter (Since class can be used without instance)
+    inline static auto isEventEmitterInitialized_{false};
 };
 
 #endif
