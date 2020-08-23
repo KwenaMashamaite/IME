f4f4eab3a24346b2b01df85656ba776a18e1edba
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-08-24 01:14:24 +0200

    Remove synchronous behaviour in function intended for asynchronous behaviour
    Former-commit-id: bdb85c75eb9452aa63f2d00f2fc58816c356ef5a

--- src/IME/utility/Timer.cpp
+++ src/IME/utility/Timer.cpp
@@ -1,22 +1,21 @@
 #include "IME/utility/Timer.h"
 #include <chrono>
 #include <thread>
-#include <future>
 
 namespace IME::Utility {
     void Timer::setTimeOut(Callback<> callback, int ms) {
-        std::async(std::launch::async, [=](){
+        std::thread([=](){
             std::this_thread::sleep_for(std::chrono::milliseconds(ms));
             callback();
-        });
+        }).detach();
     }
 
     void Timer::setInterval(Callback<bool&> callback, int ms) {
-        std::async(std::launch::async, [=, cancelInterval = false]() mutable {
+        std::thread([=, cancelInterval = false]() mutable {
             while (!cancelInterval) {
                 std::this_thread::sleep_for(std::chrono::milliseconds(ms));
                 callback(cancelInterval);
             }
-        });
+        }).detach();
     }
 }
