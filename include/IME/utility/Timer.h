db6b81d8882e71967f81627b0e17710dcfe9a71d
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-08-24 00:02:48 +0200

    Add function to execute a callback every given interval
    Former-commit-id: 8447d5b852ef0491a940fa4bb4a061ad5078a621

--- include/IME/utility/Timer.h
+++ include/IME/utility/Timer.h
@@ -12,7 +12,7 @@ namespace IME {
         class Timer {
         public:
             /**
-             * @brief Execute a callback function after a delay
+             * @brief Execute a callback function once after a delay
              * @param callback Function to execute
              * @param ms Time to wait before executing callback in milliseconds
              *
@@ -21,6 +21,19 @@ namespace IME {
              * after initiating the new thread
              */
             void setTimeOut(Callback<> callback, int ms);
+
+            /**
+             * @brief Execute a callback function repeatedly after a delay
+             * @param callback Function to execute
+             * @param ms Time to wait before executing callback in milliseconds
+             *
+             * The timing and callback execution is done in a separate thread,
+             * therefore this function is not blocking. It will return immediately
+             * after initiating the new thread. The callback function will execute
+             * forever every delay milliseconds. The interval can be stopped by
+             * setting the argument provided on invocation to false.
+             */
+            void setInterval(Callback<bool&> callback, int ms);
         };
     }
 }
--- src/IME/utility/Timer.cpp
+++ src/IME/utility/Timer.cpp
@@ -10,4 +10,13 @@ namespace IME::Utility {
             callback();
         });
     }
+
+    void Timer::setInterval(Callback<bool&> callback, int ms) {
+        std::async(std::launch::async, [=, cancelInterval = false]() mutable {
+            while (!cancelInterval) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+                callback(cancelInterval);
+            }
+        });
+    }
 }
