8da15d87c83d75b73668956b365b09ff6165d9a2
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-09-12 02:06:19 +0200

    Add AABB collision detection between sprite objects
    Former-commit-id: 87fceab5758e8d70b575d35f2a47494f103c20ef

--- include/IME/core/CollisionDetector.h
+++ include/IME/core/CollisionDetector.h
@@ -2,6 +2,7 @@
 #define COLLISIONDETECTOR_H
 
 #include "IME/core/tilemap/Tile.h"
+#include "IME/core/tilemap/TileMap.h"
 #include "IME/core/Sprite.h"
 
 namespace IME {
@@ -21,6 +22,20 @@ namespace IME {
         void tileVSTileCollision(Tile& tileOne, Tile& tileTwo, Callback<Tile&, Tile&> callback);
 
         /**
+         * @brief Check a collision between two sprite objects
+         * @param spriteOne First sprite object
+         * @param spriteTwo Second sprite object
+         * @param callback Function to execute when a collision is detected
+         *
+         * The callback function is not invoked when a collision is not taking
+         * place. When a collision is taking place, the colliding objects will
+         * be passed as arguments to the callback in the same order in which
+         * they are provided to this function
+         */
+        void spriteVsSpriteCollision(Sprite& spriteOne, Sprite& spriteTwo,
+            Callback<Sprite&, Sprite&> callback);
+
+        /**
          * @brief Check for a collision between a sprite object and a tile object
          * @param sprite Sprite object
          * @param tile Tile object
@@ -32,6 +47,20 @@ namespace IME {
          * they are provided to this function
          */
         void spriteVsTileCollision(Sprite& sprite, Tile& tile, Callback<Sprite&, Tile&> callback);
+
+        /**
+         * @brief Check for collision between a sprite object and a tilemap tile
+         * @param sprite Sprite object to check
+         * @param tileMap Tilemap to check
+         * @param callback Function to execute when a collision is detected
+         *
+         * The callback function is not invoked when a collision is not taking
+         * place. When a collision is taking place, the colliding objects will
+         * be passed as arguments to the callback in the same order in which
+         * they are provided to this function
+         */
+        void spriteVsTileMapCollision(Sprite& sprite, TileMap& tileMap,
+            Callback<Sprite&, Tile&> callback);
 
     private:
         /**
--- src/IME/core/CollisionDetector.cpp
+++ src/IME/core/CollisionDetector.cpp
@@ -11,10 +11,24 @@ namespace IME {
         }
     }
 
+    void CollisionDetector::spriteVsSpriteCollision(Sprite &spriteOne, Sprite &spriteTwo,
+        Callback<Sprite &, Sprite &> callback)
+    {
+        if (isCollideAABB(spriteOne, spriteTwo))
+            callback(spriteOne, spriteTwo);
+    }
+
     void CollisionDetector::spriteVsTileCollision(Sprite &sprite,
         Tile &tile,Callback<Sprite &, Tile &> callback)
     {
         if (tile.isCollideable() && isCollideAABB(sprite, tile))
             callback(sprite, tile);
     }
+
+    void CollisionDetector::spriteVsTileMapCollision(Sprite &sprite, TileMap &tileMap,
+        Callback<Sprite&, Tile&> callback)
+    {
+
+    }
+
 }
