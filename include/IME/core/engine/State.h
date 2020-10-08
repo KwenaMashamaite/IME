b5f4d1f02145b5f1340b22aa9cc01b56d873ab62
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2020-10-08 12:28:51 +0200

    Add state class documentation
    Former-commit-id: d5331b3f8978e86290c12b8787e401dda83be66f

--- include/IME/core/engine/State.h
+++ include/IME/core/engine/State.h
@@ -23,8 +23,8 @@ namespace IME {
         /**
          * @brief Initialize state
          *
-         * This function will be called by the engine before a state
-         * is entered
+         * This function will be called by the engine before the state is entered
+         * for the first time
          */
         virtual void initialize() = 0;
 
@@ -32,7 +32,10 @@ namespace IME {
          * @brief Update the state
          * @param deltaTime Time passed since last update
          *
-         * This function will be called by the engine once per frame
+         * This function will be called once per frame by the engine. The delta
+         * passed to it is frame rate dependent. This means that it depends on
+         * how long the current frames takes to complete. All updates that need
+         * a variable timestep must be defined in this function
          */
         virtual void update(float deltaTime) = 0;
 
@@ -40,43 +43,58 @@ namespace IME {
          * @brief Update state in fixed time steps
          * @param deltaTime Time passed since last update
          *
-         * This function will be called by the engine multiple times
-         * per frame
+         * This function will be called multiple times per frame by the engine.
+         * The delta passed to it is always the same. All update that require a
+         * fixed timestep must be defined in this function. This function allows
+         * updates to be the same regardless of how fast or slow the computer is
          */
         virtual void fixedUpdate(float deltaTime) = 0;
 
         /**
          * @brief Render the state on a render target
          * @param renderTarget Target to render state on
+         *
+         * This function will be called once per frame by the engine after all
+         * events have been handled and all updates have performed for the
+         * current frame
          */
         virtual void render(Graphics::Window &renderTarget) = 0;
 
         /**
          * @brief Pause the state
          *
-         * This function will be called by the game engine when a state
-         * change is requested and this state is the current state
+         * This function will be called by the game engine on the current state
+         * when a state change is requested. This function allows a state to pause
+         * itself such that when it is returned to, it can resume where it left of
+         * instead of being reinitialized
          */
         virtual void pause() = 0;
 
         /**
          * @brief Handle an event
          * @param event Event to handle
+         *
+         * This function wil be called by the engine at the start of the current
+         * frame
          */
         virtual void handleEvent(sf::Event event) = 0;
 
         /**
          * @brief Resume a paused state
          *
-         * This function will be called by the engine when it switches
-         * back to this state
+         * This function will be called by the game engine when a previously
+         * initialised state is returned to @see initialize() and pause()
          */
         virtual void resume() = 0;
 
         /**
          * @brief Check if a state is initialized or not
-         * @return True if state is initialized or false if state is
-         * not initialized
+         * @return True if state is initialized or false if state is not initialized
+         *
+         * This function will be called by the engine when a state push or a state
+         * pop was requested in the previous frame. This ensures that a previously
+         * initialized state is resumed instead of being reinitialized and vice
+         * versa @see initialize(), resume() and pause()
          */
         virtual bool isInitialized() const = 0;
 
