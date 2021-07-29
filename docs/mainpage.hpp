1c85fb3216f0d6ba3b7be10ab7c59605acbce660
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2021-07-29 20:13:27 +0200

    Update main page documentation for v2.2.0

--- docs/mainpage.hpp
+++ docs/mainpage.hpp
@@ -8,13 +8,9 @@
 /// \section example Getting started
 /// IME is state based, which means that the game flow is controlled by pushing
 /// and popping game states at appropriate times. In IME everything lives in a
-/// ime::Scene. To get started, you must create the initial state (or scene),
-/// then create the engine and push the initial scene to it. Here are the three
-/// steps we need to do:
-///
-///     1. Create a game state
-///     2. Instantiate the game engine
-///     3. Push the initial state/scene
+/// ime::Scene. To get started, you must create a scene (or state), then create
+/// the ime::Engine and initialize it. Finally, add a scene or scenes to the
+/// engine and run it. This will start the main game loop.
 ///
 /// Here is the minimal and complete code to achieve the above steps:
 ///  @code
@@ -22,12 +18,12 @@
 ///  // once you know whats going on
 ///  #include <IME/IME.h>
 ///
-/// // Step 1: Create a game state/scene
-/// // You define the behavior of the game by overriding ime::Scene functions.
-/// // These functions will be called by the engine at appropriate times. Scene
+/// // Step 1: Create a scene (or state)
+/// // You define the behavior of your game by overriding ime::Scene functions.
+/// // These functions will be called by the engine at appropriate times. ime::Scene
 /// // methods are overridden on a need to use case.
 ///
-/// // This scene displays a message at the centre of the screen
+/// // This scene displays a welcome message at the centre of the screen
 /// class StartUpScene : public ime::Scene {
 ///   public:
 ///       void onEnter() override {
@@ -50,26 +46,27 @@
 /// {
 ///    // Step 2: Creating the game engine
 ///
-///    // 2.1 - First we need to specify settings required by the engine (optional).
-///    //       If a preference entry is not given, the engine will use a default value
+///    // 2.1 - First we need to specify settings required by the engine. These
+///    //       settings are optional. When not specified the engine will use
+///    //       default values
 ///    ime::PrefContainer settings;
-///    settings.addPref({"WINDOW_TITLE", ime::Preference::Type::String, std::string("IME Demo App")});
-///    settings.addPref({"WINDOW_WIDTH", ime::Preference::Type::Int, 600});
-///    settings.addPref({"WINDOW_HEIGHT", ime::Preference::Type::Int, 600});
-///    settings.addPref({"WINDOW_ICON", ime::Preference::Type::String, std::string("assets/images/icon.png")});
-///    settings.addPref({"FULLSCREEN", ime::Preference::Type::Bool, false});
-///    settings.addPref({"FPS_LIMIT", ime::Preference::Type::Int, 60});
-///    settings.addPref({"V_SYNC", ime::Preference::Type::Bool,  true});
-///    settings.addPref({"FONTS_DIR", ime::Preference::Type::String, std::string("assets/fonts/")});
-///    settings.addPref({"TEXTURES_DIR", ime::Preference::Type::String, std::string("assets/textures/")});
-///    settings.addPref({"SOUND_EFFECTS_DIR", ime::Preference::Type::String, std::string("assets/soundEffects/")});
-///    settings.addPref({"MUSIC_DIR", ime::Preference::Type::String, std::string("assets/music/")});
+///    settings.addPref({"WINDOW_TITLE", ime::PrefType::String, std::string("IME Demo App")});
+///    settings.addPref({"WINDOW_WIDTH", ime::Pref::Type::Int, 600});
+///    settings.addPref({"WINDOW_HEIGHT", ime::Pref::Type::Int, 600});
+///    settings.addPref({"WINDOW_ICON", ime::Pref::Type::String, std::string("assets/images/icon.png")});
+///    settings.addPref({"FULLSCREEN", ime::Pref::Type::Bool, false});
+///    settings.addPref({"FPS_LIMIT", ime::Pref::Type::Int, 60});
+///    settings.addPref({"V_SYNC", ime::Pref::Type::Bool,  true});
+///    settings.addPref({"FONTS_DIR", ime::Pref::Type::String, std::string("assets/fonts/")});
+///    settings.addPref({"TEXTURES_DIR", ime::Pref::Type::String, std::string("assets/textures/")});
+///    settings.addPref({"SOUND_EFFECTS_DIR", ime::Pref::Type::String, std::string("assets/soundEffects/")});
+///    settings.addPref({"MUSIC_DIR", ime::Pref::Type::String, std::string("assets/music/")});
 ///
-///    // Step 2.2 - Instantiating the engine
+///    // Step 2.2 - Instantiating and initializing the engine
 ///    ime::Engine engine = ime::Engine("Name of game here", settings);
 ///    engine.initialize();
 ///
-///    // Step 3: Push the initial state/scene and start the game loop
+///    // Step 3: Push the initial state and start the game loop
 ///    engine.pushScene(std::make_unique<StartUpScene>());
 ///    engine.run();
 ///
