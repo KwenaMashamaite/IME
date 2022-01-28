d86de81865643f9fda3cbda5b9299a79755359ed
Kwena Mashamaite <kwena.mashamaite1@gmail.com>
2022-01-28 13:38:30 +0200

    Delete 'ime::TextureAtlas' class

--- include/IME/Graphics.h
+++ include/IME/Graphics.h
@@ -36,7 +36,6 @@
 #include "IME/graphics/Sprite.h"
 #include "IME/graphics/SpriteImage.h"
 #include "IME/graphics/SpriteSheet.h"
-#include "IME/graphics/TextureAtlas.h"
 #include "IME/graphics/Texture.h"
 #include "IME/graphics/Tile.h"
 #include "IME/graphics/Drawable.h"
